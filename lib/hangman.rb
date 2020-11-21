d148b85a686012790e79ae99e17630628c73c504
Jake <jakegreen2016@gmail.com>
2020-11-20 20:05:19 -0900

    remove \r and \n from sample array

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,9 +3,16 @@ dictionary = File.readlines "5desk.txt"
 # retrieve a random word from list (61407 words!)
 word = dictionary.sample
 puts word
+word = word.split("")
+remove_last_two = word.pop(2)
+print word
+puts ""
 
 # display underscores for each letter of the word AND display hangman picture
+hidden_letters = Array.new
+word.map {|i| hidden_letters.push("_")}
 
+print hidden_letters
 # prompt the player to enter a letter
 
   # test to make sure it's a letter
