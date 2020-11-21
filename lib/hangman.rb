d1d5836bb46fb5f38bac0036869083555553cb12
Jake <jakegreen2016@gmail.com>
2020-11-20 18:24:10 -0900

    write pseudo-code

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,21 @@
 tester = File.readlines "5desk.txt"
 
 tester.each do |word| 
-    puts word
+  puts word
 end
+
+# retrieve a random word from list
+
+# display underscores for each letter of the word AND display hangman picture
+
+# prompt the player to enter a letter
+
+  # test to make sure it's a letter
+
+# if a letter enter matches one from the word, display it instead of the underscore
+
+# if a letter doesn't match any from the word, add another piece of the "hang man"
+
+# contine the process until player guesses the word or makes a hangman
+
+# declare end of game
