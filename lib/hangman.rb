eb16e1b64eea2242ced74d4bdc17e27faf9e71d4
Jake <jakegreen2016@gmail.com>
2020-11-21 11:59:31 -0900

    run the game multiple rounds

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -10,21 +10,24 @@ hidden_letters = Array.new
 word.map {|i| hidden_letters.push("_")}
 print hidden_letters
 puts ""
-# prompt the player to enter a letter
-puts "This is a test round of my hangman game! ENTER a lowercase LETTER \n"
-letter_guess = gets.chomp
-# if a letter enter matches one from the word, display it instead of the underscore
-i = 0 
-while i < word.length do 
-  if letter_guess == word[i] then
-    hidden_letters[i] = letter_guess
+
+# run multiple rounds
+until hidden_letters == word do
+  # prompt the player to enter a letter
+  puts "This is a test round of my hangman game! ENTER a lowercase LETTER \n"
+  letter_guess = gets.chomp
+  # if a letter enter matches one from the word, display it instead of the underscore
+  i = 0 
+  while i < word.length do 
+    if letter_guess == word[i] then
+      hidden_letters[i] = letter_guess
+    end
+    i += 1
   end
-  i += 1
+  puts ""
+  print hidden_letters
+  puts ""
 end
-puts ""
-print hidden_letters
-puts ""
-puts "Successful Round!"
 # if a letter doesn't match any from the word, add another piece of the "hang man"
 
 # contine the process until player guesses the word or makes a hangman
