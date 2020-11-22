fa63c67febeedb31fd13a0fbce3d5419bbd95358
Jake <jakegreen2016@gmail.com>
2020-11-22 14:08:50 -0900

    enable player to save their game to a file

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -14,7 +14,6 @@ puts ""
 incorrect_guesses = 0
 # run multiple rounds
 until ( (hidden_letters == word) || (incorrect_guesses == 6) ) do
-  puts incorrect_guesses
   changes = 0 
   # prompt the player to enter a letter
   puts "This is a test round of my hangman game! ENTER a lowercase LETTER \n"
@@ -26,13 +25,26 @@ until ( (hidden_letters == word) || (incorrect_guesses == 6) ) do
       hidden_letters[i] = letter_guess
       changes += 1
     end
-    p changes
+    changes
     i += 1
   end
   incorrect_guesses += 1 if changes == 0
   puts ""
   print hidden_letters
   puts "Incorrect guesses #{incorrect_guesses}"
+
+  puts "Save progress? (y/n)"
+  answer = gets.chomp
+  until ( (answer == "y") || (answer == "n") ) do 
+    puts "Not a valid choice, enter 'y' or 'n'."
+    answer = gets.chomp
+  end
+  if answer == "y" then
+    fd = IO.sysopen('/home/jakeo/the_odin_project/files-and-serialization/File-I-O-and-Serialization/save_file.txt', "w")
+    a = IO.new(fd, "w")
+    a.puts incorrect_guesses
+    a.print hidden_letters
+  end
 end
 # if a letter doesn't match any from the word, add another piece of the "hang man"
 
